c89ab20a64931d7f124de52dad1016d2e6b01dfb
Saoud Alfares <sfares85@gmail.com>
2019-07-10 18:54:34 +0300

    test passed

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	opening_time = models.TimeField(auto_now=False, auto_now_add=False)
+	closing_time = models.TimeField(auto_now=False, auto_now_add=False)
+
+	def __str__(self):
+		return self.name
